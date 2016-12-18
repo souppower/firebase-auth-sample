5a19e303fa396597c756ec8233f0fa568a5f2558
souppower <ryo.kubota@topgate.co.jp>
2016-12-19 02:38:20 +0900

    feature(front): add authentication feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user = {};
+  constructor(public af: AngularFire) {
+    this.af.auth.subscribe(user => {
+      if(user) {
+        this.user = user;
+        console.log(this.user);
+      }
+      else {
+        this.user = {};
+      }
+    });
+  }
+
+  login(from: string) {
+    this.af.auth.login({
+      provider: this._getProvider(from)
+    });
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  private _getProvider(from: string) {
+    switch(from){
+      case 'twitter': return AuthProviders.Twitter;
+      case 'google': return AuthProviders.Google;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  AngularFireModule,
+  AuthMethods,
+  AuthProviders
+} from "angularfire2";
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKQN9XrUs0y_ETc1IaLjrrML4z8K7y03k",
+    authDomain: "login-sample-d8370.firebaseapp.com",
+    databaseURL: "https://login-sample-d8370.firebaseio.com",
+    storageBucket: "login-sample-d8370.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Twitter,
+      method: AuthMethods.Popup
+      // method: AuthMethods.Redirect
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
